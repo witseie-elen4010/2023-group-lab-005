99a1f20d4051ac049a71ed12a4897681f12704c8
Christina Mathipa <mathipachristina@gmail.com>
2023-05-10 21:42:50 +0200

    Set up the server and fixed the file structure

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`)
+})
