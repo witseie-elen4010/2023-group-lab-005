670e4838602591699dfa390047e4c062c5149c1d
Christina Mathipa <mathipachristina@gmail.com>
2023-05-11 09:31:05 +0200

    Added the landing page

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+app.use('/public/', express.static('./public'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('LandingPage')
+})
+
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`)
+})
