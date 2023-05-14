341b8518a10536f1dbc8ff332a86ae8f426610a6
Christina Mathipa <mathipachristina@gmail.com>
2023-05-14 21:33:16 +0200

    fixed the lock file

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use('/public/', express.static('./public'));
+app.use('/public/', express.static('./public'))
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('LandingPage');
-});
+  res.render('LandingPage')
+})
 
 app.listen(port, () => {
-  console.log(`server at http://localhost:${port}`);
-});
+  console.log(`server at http://localhost:${port}`)
+})
