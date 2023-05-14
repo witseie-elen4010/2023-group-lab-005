932823e0c59f620ce2a53b9d5cc11e6dfc0f51aa
samacohen <2168179@students.wits.ac.za>
2023-05-14 14:46:21 +0200

    able to pass in login information from the JS file to EJS file during rendering

--- src/routes/studentRouter.js
+++ src/routes/studentRouter.js
@@ -4,9 +4,15 @@ const express = require('express')
 
 const studentRouter = express.Router()
 
+//An object to dynamically interact with some of the EJS elements during rendering
+const LoginInfo = {
+    userType: 'Student',
+    errorText: ''   //This will contain error texts to communicate wtih page during login
+}
+
 //Where the page must go to if the login button is pressed 
 studentRouter.get('/login-student', (req,res)=>{
-    res.render('studentSignIn')
+    res.render('studentSignIn', {signininfo: LoginInfo})
 })
 
 // Add a post method for recieving data from the login page
