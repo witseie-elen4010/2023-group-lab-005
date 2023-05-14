0d9ea5405b5980231bcd7bc2638847b089c5fa5f
samacohen <2168179@students.wits.ac.za>
2023-05-14 14:25:52 +0200

    able to post log in data from sign in  page to the router

--- src/routes/studentRouter.js
+++ src/routes/studentRouter.js
@@ -1,11 +1,20 @@
 // Student router
 
 const express = require('express')
+
 const studentRouter = express.Router()
 
 //Where the page must go to if the login button is pressed 
 studentRouter.get('/login-student', (req,res)=>{
     res.render('studentSignIn')
+})
+
+// Add a post method for recieving data from the login page
+studentRouter.post('/login', function(req,res){
+    console.log(req.body.email)
+
+    //instruct the page to return the student loging page when its done executing 
+    res.redirect(req.baseUrl + '/login-student')
 })
 
 module.exports = studentRouter
--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+
+//This is a library that will allow our program to parse html/ejs content as json objects
+const bodyParser = require('body-parser')
+
 const studentRouter = require('./routes/studentRouter')
 
 const app = express();
@@ -6,12 +10,14 @@ const port = process.env.PORT || 5000;
 
 app.use('/public/', express.static('./public'));
 
+//instruct our app to use json bodyu parser 
+app.use(bodyParser.json()).use(bodyParser.urlencoded({extended:true}))
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('LandingPage');
 });
-
 
 // This tells the server to include the student router 
 app.use('/',studentRouter)
