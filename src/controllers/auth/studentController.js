8030a86d77c96d6e62503acbff0e270e44fca6a8
samacohen <2168179@students.wits.ac.za>
2023-05-16 11:20:46 +0200

    Login authentication in student controller

--- src/controllers/auth/studentController.js
+++ src/controllers/auth/studentController.js
@@ -42,3 +42,44 @@ exports.postSignUp = async (req, res) => {
     res.status(500).render('error', { errorMessage: 'Server error' })
   }
 }
+
+//Login authentication
+
+// Render the login form
+exports.getLogin = (req, res) => {
+  res.render('./studentSignin');
+};
+
+// Handle login form submission
+exports.postLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if a student with the provided email exists
+    const student = await StudentModel.findOne({ email });
+    if (!student) {
+      req.flash('error', 'Invalid email or password');
+      return res.redirect('login-student');
+    }
+
+    // Compare the provided password with the stored hashed password
+    const passwordMatch = await bcrypt.compare(password, student.password);
+    if (!passwordMatch) {
+      req.flash('error', 'Invalid email or password');
+      return res.redirect('login-student');
+    }
+
+    // Generate JWT token and set it as a cookie
+    const token = jwt.sign({ studentId: student._id }, process.env.JWT_SECRET);
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    // Redirect to the dashboard page
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).render('error', { errorMessage: 'Server error' });
+  }
+};
