17b7039cbd8cb97ab8fcc090a63ee5b4a0f5a621
Taine Elliott <2129200@students.wits.ac.za>
2023-05-16 17:07:32 +0200

    Made a model for the lecturer

---
+++ src/models/lecturerModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const lecturerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  availability: [
+    {
+      day: {
+        type: String,
+        enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+        required: true,
+      },
+      slots: [
+        {
+          startTime: {
+            type: String,
+            required: true,
+          },
+          endTime: {
+            type: String,
+            required: true,
+          },
+          maxStudents: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+    },
+  ],
+});
+
+const Lecturer = mongoose.model("Lecturer", lecturerSchema);
+
+module.exports = Lecturer;
