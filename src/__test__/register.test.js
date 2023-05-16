efb2c89dfa5486e12e809c7b14089c2548fe028e
Taine Elliott <2129200@students.wits.ac.za>
2023-05-16 17:57:54 +0200

    lecturer-sign in  test cases passed

--- src/__test__/register.test.js
+++ src/__test__/register.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const app = require('../server')
 const Student = require('../models/studentModel')
+const Lecturer = require('../models/lecturerModel')
 require('dotenv').config()
 
 describe('Student registration', () => {
@@ -79,4 +80,41 @@ describe('Sign-in page', () => {
     expect(response.headers.location).toBe('/'); // Expecting a redirect to the dashboard page
     // You can also check for additional things like a successful cookie being set, etc.
   });
+})
+
+describe('Lecturer registration', () => {
+  afterEach(async () => {
+    // Clean up the database after each test
+    await Lecturer.deleteMany()
+  }, 100000) // Add timeout option here with a higher value in milliseconds
+
+  test('New lecturer can see sign-up form with name, email, and password fields', async () => {
+    const response = await request(app).get('/register-lecturer')
+    expect(response.statusCode).toBe(200)
+    expect(response.text).toContain('name')
+    expect(response.text).toContain('email')
+    expect(response.text).toContain('password')
+  })
+
+  test('New lecturer can create an account and receive a confirmation email', async () => {
+    const lecturerData = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password',
+      confirmPassword: 'password'
+    }
+    const server = app.listen() // start the server and get the server instance
+    const response = await request(server)
+      .post('/register-lecturer')
+      .send(lecturerData)
+
+    // Check that the student was saved to the database
+    const savedLecturer = await Lecturer.findOne({ email: lecturerData.email })
+    expect(savedLecturer).not.toBeNull()
+
+    // Check that a confirmation email was sent
+    expect(response.statusCode).toBe(302) // redirect after successful registration
+    // ... add code to check that a confirmation email was sent ...
+  })
+
 })
