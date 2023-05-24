7502046452e9ee7b8de40dcd02fc7e8bd3c076c5
samacohen <2168179@students.wits.ac.za>
2023-05-24 10:48:52 +0200

    Added functionality to consultationController

--- src/controllers/consultationController.js
+++ src/controllers/consultationController.js
@@ -5,3 +5,59 @@ exports.renderConsultationSetup = (req, res) => {
   // Render the consultation setup view
   res.render("consultationSetup", { title: "Consultation Setup" });
 };
+
+
+// Controller method for handling the submission of the consultation setup form
+exports.createConsultation = async (req, res) => {
+    try {
+      // Retrieve form data from the request body
+      const { attendees, lecturerEmail, maxStudents, day, startTime, endTime } =
+        req.body;
+  
+      // Find the lecturer and update their availability
+      const lecturer = await Lecturer.findOne({ email: lecturerEmail });
+  
+      if (!lecturer) {
+        console.log("Error: Lecturer not found");
+        req.flash("error", "Lecturer not found");
+        return res.redirect("/see-lecturer-availability");
+      }
+  
+      let slotFound = false;
+  
+      
+  
+      if (!slotFound) {
+        console.log("Error: Slot not found or already booked");
+        req.flash("error", "Failed to book slot");
+        return res.redirect("/see-lecturer-availability");
+      }
+  
+      await lecturer.save();
+      console.log("Slot booked successfully");
+      req.flash("success", "Slot booked successfully");
+  
+      // Split the attendees string into an array of email addresses
+      const attendeesArray = attendees.split(",").map((email) => email.trim());
+  
+      // Create a new consultation object
+      const consultation = new Consultation({
+        attendees: attendeesArray,
+        lecturerEmail,
+        maxStudents,
+        day,
+        startTime,
+        endTime,
+      });
+  
+      // Save the consultation to the database
+      await consultation.save();
+      console.log("Consultation has been set up successfully");
+      req.flash("success", "Consultation has been set up successfully");
+    } catch (err) {
+      console.log("Error: Failed to set up consultation", err);
+      req.flash("error", "Failed to set up consultation");
+    }
+  
+    res.redirect("/see-lecturer-availability");
+  };
