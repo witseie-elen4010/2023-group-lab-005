fbd598a73723d66b1abe1548cca005da65ac7d88
Sam Cohen <cohensam51@gmail.com>
2023-05-28 14:43:05 +0300

    student can signup and password is hashed, then can sign in

--- src/controllers/auth/studentController.js
+++ src/controllers/auth/studentController.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const Student = require("../../models/studentModel");
 const logger = require("../../controllers/logController");
+const bcrypt = require('bcryptjs')
 
 // Render the sign-up form
 exports.getSignUp = (req, res) => {
@@ -26,15 +27,8 @@ exports.postSignUp = async (req, res) => {
     }
 
     // Create a new user with the provided details
-    const student = new Student({ name, email, password });
+    const student = new Student({ name, email, password: bcrypt.hashSync(password) });
     await student.save();
-
-    // // Generate JWT token and set it as a cookie
-    // const token = jwt.sign({ studentId: student._id }, process.env.JWT_SECRET);
-    // res.cookie("jwt", token, {
-    //   httpOnly: true,
-    //   secure: process.env.NODE_ENV === "production",
-    // });
 
      // Generate JWT token
      const token = jwt.sign(
@@ -75,8 +69,8 @@ exports.postSignIn = async (req, res) => {
     }
 
     // Check if provided password is correct
-    const isPasswordValid = password === student.password;
-    if (!isPasswordValid) {
+    
+    if (!(bcrypt.compareSync(password, student.password))) {
       req.flash("error", "Invalid email or password");
       return res.status(401).redirect("/login-student");
     }
