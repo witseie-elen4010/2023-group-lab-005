319fbdbd0bed67d80e8d04cfc45e5c21441065da
Taine Elliott <2129200@students.wits.ac.za>
2023-05-31 12:56:55 +0200

    Changed the implimentation of the notification

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ const consultationRoutes = require("./routes/consultationRouter");
 const connectDB = require("./db");
 const flash = require("express-flash");
 const session = require("express-session");
+const Agenda = require("agenda"); 
+const mailer = require("./controllers/emailController");
+const Consultation = require("./models/consultationModel");
+
 
 app.use("/public/", express.static("./public"));
 
@@ -40,8 +44,76 @@ app.use(studentAuthRoutes);
 app.use(lecturerAuthRoutes);
 app.use(studentLogRoutes);
 app.use(consultationRoutes);
+
+
+// Start Agenda
+const agenda = new Agenda({
+  db: { 
+    address: process.env.MONGO_URI,
+    collection: "jobs",
+  },
+});
+
+ // Define the send email job
+ agenda.define("send email", async (job) => {
+  const recipientEmail = job.attrs.data.recipientEmail;
+  const message = job.attrs.data.message;
+
+  try {
+    await mailer.sendEmail(recipientEmail, message);
+  } catch (err) {
+    console.error("Error sending email:", err);
+  }
+});
+
+// Start agenda process every two hours
+agenda.processEvery("2 hours");
+
+
+// Start agenda and schedule the consultation email job
+agenda.start().then(() => {
+  console.log("Agenda is running");
+  sendConsultationEmails(); // Schedule consultation email job
+});
+
+const sendConsultationEmails = async () => {
+  try {
+    // Get the current day
+    const currentDay = new Date().toLocaleDateString("en-US", { weekday: "long" });
+
+    // Find consultations with the same day as the current day
+    const consultations = await Consultation.find({ day: currentDay });
+
+    // Iterate over each consultation
+    for (const consultation of consultations) {
+      const lecturerEmail = consultation.lecturerEmail;
+  
+      const message = "Hello, this is a scheduled email for the consultation.";
+
+      // Send email to lecturer
+      await mailer.sendEmail(lecturerEmail, message);
+
+     
+    }
+
+    console.log("Emails sent successfully");
+  } catch (err) {
+    console.error("Error sending emails:", err);
+  }
+};
+
+
+
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
 });
 
 module.exports = app;
+
