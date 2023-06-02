25fc0d9ad4be1f4835aabb59aeb8ed409405fc8b
Christina Mathipa <mathipachristina@gmail.com>
2023-06-02 06:16:12 +0200

    improved admin auth security

--- src/controllers/auth/adminController.js
+++ src/controllers/auth/adminController.js
@@ -1,100 +1,117 @@
 const jwt = require("jsonwebtoken");
 const Admin = require("../../models/adminModel");
-
+const mailer = require("../emailController");
+const Lecturer = require("../../models/lecturerModel");
+const Student = require("../../models/studentModel");
+const bcrypt = require("bcryptjs");
+let generatedPassword = "";
 // Render the admin sign-in form
 exports.getAdminSignIn = (req, res) => {
   res.render("./auth/admin/signin");
 };
 
-// Handle admin sign-in form submission
-// Handle admin sign-in form submission
-exports.postAdminSignIn = async (req, res) => {
-    const { email, password } = req.body;
-  
-    // Hardcoded admin credentials
-    const adminEmail = 'admin@gmail.com';
-    const adminPassword = 'Adm!n123';
-  
-    try {
-      // Check if the provided email matches the hardcoded admin email
-      if (email !== adminEmail) {
-        req.flash('error', 'Invalid email or password');
-        return res.status(401).redirect('/login-admin');
-      }
-  
-      // Check if the provided password matches the hardcoded admin password
-      if (password !== adminPassword) {
-        req.flash('error', 'Invalid email or password');
-        return res.status(401).redirect('/login-admin');
-      }
-  
-      // Generate JWT token
-      const token = jwt.sign(
-        { adminId: 'admin123', email: adminEmail },
-        process.env.JWT_SECRET
-      );
-  
-      // Store the token, ID, and email in the session
-      req.session.token = token;
-      req.session.adminId = 'admin123';
-      req.session.email = adminEmail;
-  
-      // Redirect to the admin dashboard page
-      res.redirect('/admin-dashboard');
-    } catch (err) {
-      console.error(err);
-      res.status(500).render('error', { errorMessage: 'Server error' });
-    }
-  };
-  
-  // controllers/adminController.js
+// Generate a random password
+function generateRandomPassword(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+}
+// Controller for generating a password
+exports.generatePassword = async (req, res) => {
+  const { email } = req.body;
 
-const Lecturer = require('../../models/lecturerModel');
-const Student = require('../../models/studentModel');
+  try {
+    // Check if the submitted email matches the specific admin email
+    if (email !== "itchiraira@gmail.com") {
+      req.flash("error", "Invalid email");
+      return res.status(401).redirect("/generate-password");
+    }
+
+    // Generate a random password
+    const length = 10; // Adjust the length of the generated password as needed
+    generatedPassword = generateRandomPassword(length);
+
+    // Send the generated password via email
+    await mailer.sendEmail(email, generatedPassword);
+
+    // Hash the generated password
+    const hashedPassword = await bcrypt.hash(generatedPassword, 10);
+
+    // Redirect to the normal sign-in page with a success message and the generated password as a query parameter
+    req.flash("success", "A generated password has been sent to your email");
+    res.redirect(`/login-admin`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).render("error", { errorMessage: "Server error" });
+  }
+};
+
+// Controller for signing in
+exports.postAdminSignIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (password !== generatedPassword) {
+      req.flash("error", "Invalid email or password");
+      return res.status(401).redirect("/login-admin");
+    }
+
+    // Authentication successful
+    req.flash("success", "Sign-in successful");
+    res.redirect("/admin-dashboard");
+  } catch (err) {
+    console.error(err);
+    res.status(500).render("error", { errorMessage: "Server error" });
+  }
+};
+
+exports.getGeneratePassword = (req, res) => {
+  res.render("./auth/admin/generatePassword");
+};
 
 exports.getDashboard = async (req, res) => {
   try {
     const lecturers = await Lecturer.find({});
     const students = await Student.find({});
 
-    res.render('adminDashboard', { lecturers, students });
+    res.render("adminDashboard", { lecturers, students });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
 
-// controllers/adminController.js
-
-// ...
-
 exports.deleteLecturer = async (req, res) => {
-    try {
-      const result = await Lecturer.deleteOne({ _id: req.params.id });
-  
-      if (result.deletedCount === 0) {
-        return res.status(404).json({ msg: 'Lecturer not found' });
-      }
-  
-      res.json({ msg: 'Lecturer removed' });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
+  try {
+    const result = await Lecturer.deleteOne({ _id: req.params.id });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ msg: "Lecturer not found" });
     }
-  };
-  
-  exports.deleteStudent = async (req, res) => {
-    try {
-      const result = await Student.deleteOne({ _id: req.params.id });
-  
-      if (result.deletedCount === 0) {
-        return res.status(404).json({ msg: 'Student not found' });
-      }
-  
-      res.json({ msg: 'Student removed' });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
+
+    res.json({ msg: "Lecturer removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.deleteStudent = async (req, res) => {
+  try {
+    const result = await Student.deleteOne({ _id: req.params.id });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ msg: "Student not found" });
     }
-  };
-  
+
+    res.json({ msg: "Student removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
